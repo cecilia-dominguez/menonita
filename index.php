fb61c6be527c4370feab0e5533021b3d61d55624
kebelandrade <andradekebel97@gmail.com>
2019-05-20 18:33:42 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+    <title>Comité Juvenil Nacional Menonita</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php require 'menu.html'?>
+<section id="seccion1">
+    <div class="bd-example" id="car">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/paz.jpg" class="d-block w-100" alt="imagen1">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Retiro Juvenil Nacional Menonita 2019</h5>
+                        <p>Stop Wars, Jesús Principe de Paz</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/2018.jpg" class="d-block w-100" alt="imagen2">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Retiro Juvenil Nacional Menonita 2018</h5>
+                        <p>Precaución, Jesús Trabajando</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/car.jpg" class="d-block w-100" alt="imagen3">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</section>
+<section id="seccion2">
+<div id="containe-padre">
+    <div class="texto">
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores eos et excepturi fuga iure labore laboriosam nihil nostrum possimus provident, quibusdam quo reiciendis sit, suscipit temporibus, veritatis. Exercitationem, iure.</h3>
+    </div>
+</div>
+</section>
+<section id="seccion3">
+
+</section>
+</body>
+</html>
